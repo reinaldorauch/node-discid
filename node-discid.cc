fbc6044f9efc007cf7d2d62d22769e718ab7c1aa
Reinaldo A. C. Rauch <reinaldorauch@gmail.com>
2017-06-12 15:23:35 -0300

    Tentativa de uso da lib

--- node-discid.cc
+++ node-discid.cc
@@ -1,9 +1,22 @@
 #include <node.h>
+#include <discid/discid.h>
 
-void GenerateTocFromDevice(const v8::FunctionCallackInfo<v8::Value>& args) {
+void GenerateTocFromDevice(const v8::FunctionCallbackInfo<v8::Value>& args) {
     v8::Isolate* isolate = args.GetIsolate();
-    auto message = v8::String::NewFromUtf8(isolate, "TOC");
-    args.GetReturnValue().Set(message);
+
+    DiscId *disc = discid_new();
+
+    if (discid_read_sparse(disc, "/dev/cdrom", 0) == 0) {
+        isolate->ThrowException(v8::Exception::Error(
+            v8::String::NewFromUtf8(isolate, discid_get_error_msg(disc))
+        ));
+        discid_free(disc);
+        return;
+    }
+
+    auto id = v8::String::NewFromUtf8(isolate, discid_get_id(disc));
+    args.GetReturnValue().Set(id);
+    discid_free(disc);
 }
 
 void Initialize(v8::Local<v8::Object> exports) {
